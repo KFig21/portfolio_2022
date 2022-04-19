3f0a9418fbdf2321f9739cbab63124e42428d7cd
Kevin <kfig21@gmail.com>
2022-04-19 01:55:49 -0400

    new background

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Background.scss";
-import lightBG from "./lightBG.mp4";
+// import lightBG from "./lightBG.mp4";
+import backgroundVideo from "./pb3.mp4";
 
 export default function Background(theme) {
   return (
     <div className="video-container">
       <video autoPlay loop muted className={theme.theme.type}>
-        <source src={lightBG} type="video/mp4" />
+        <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
     </div>
